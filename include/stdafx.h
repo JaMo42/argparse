3fff14ca431967e99ccb628c660831f5fc8404c9
JaMo42 <30950798+JaMo42@users.noreply.github.com>
2019-10-30 21:01:57 +0100

    add stdafx

---
+++ include/stdafx.h
@@ -0,0 +1,15 @@
+#ifndef STDAFX_H
+#define STDAFX_H
+
+#include <cstdio>
+
+#include <string>
+#include <string_view>
+#include <vector>
+#include <map>
+#include <forward_list>
+#include <optional>
+
+#include <algorithm>
+
+#endif /* !STDAFX_H */
